e343a0aae56f77721d4e0f4a9b4a0c7426a002e2
Hexin Wang <hexinw@nvidia.com>
2025-09-19 15:36:29 -0700

    .

--- src/nvidia_resiliency_ext/fault_tolerance/_ft_rendezvous.py
+++ src/nvidia_resiliency_ext/fault_tolerance/_ft_rendezvous.py
@@ -1301,6 +1301,33 @@ class FtRendezvousHandler(RendezvousHandler):
             log.debug("RendezvousInfo not available, using tuple format")
             return store, rank, world_size
 
+    def is_closed(self) -> bool:
+        """See base class."""
+        try:
+            with self._heartbeat_lock:
+                self._state_holder.sync()
+
+                return self._state_holder.state.closed
+
+        except Exception as e:
+            self._record(
+                message=f"{type(e).__name__}: {str(e)}",
+                node_state=NodeState.FAILED,
+            )
+            raise
+
+    def set_closed(self) -> None:
+        """See base class."""
+        try:
+            with self._heartbeat_lock:
+                self._close()
+        except Exception as e:
+            self._record(
+                message=f"{type(e).__name__}: {str(e)}",
+                node_state=NodeState.FAILED,
+            )
+            raise
+
     def num_nodes_waiting(self) -> int:
         """See base class."""
         try:
